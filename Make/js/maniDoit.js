049d048e591e8ab5a0db80ee5ec02817d78fa46b
sohiLee <lsohi2468@gmail.com>
2021-01-21 01:44:41 +0900

    loadName

--- Make/js/maniDoit.js
+++ Make/js/maniDoit.js
@@ -1,5 +1,19 @@
-function init(){
+// 이름 저장
+function loadName(){
+ const h1 = document.querySelector('h1');
+ const a = "1122"
+ h1.innerHTML = `${a}test`;
+//  h1.innerText = `${a}test`
+}
 
+
+
+
+
+
+
+function init(){
+loadName();
 }
 
 init();
