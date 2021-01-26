dd5a4f0867bbea35eb892c2976a9c008218e4a4b
sohiLee <lsohi2468@gmail.com>
2021-01-26 23:45:26 +0900

    preventdefault

--- Make/js/maniDoit.js
+++ Make/js/maniDoit.js
@@ -1,17 +1,20 @@
 const h1 = document.querySelector('h1');
-const form = document.querySelector('.title');
+const nameForm = document.querySelector('.title');
+const nameInput = nameForm.querySelector('input');
 const NAME_LS = "name";
 const userName = localStorage.getItem(NAME_LS);
 
 
 
-function handelName(){
-    console.log('test')
+function handelName(e){
+    e.preventDefault();
+    const nameValue = nameInput.value;
+    console.log('a');
 }
 
 function writeName(){
 // 이름 입력
-form.addEventListener('submit',handelName);
+nameForm.addEventListener('submit',handelName);
 }
 
 
