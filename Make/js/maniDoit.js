ce76f19159cf300114c1d3c9cafeb1b75d50bc23
LSH <lsohi2468@gmail.com>
2021-01-27 18:24:01 +0900

    add name

--- Make/js/maniDoit.js
+++ Make/js/maniDoit.js
@@ -5,30 +5,31 @@ const NAME_LS = "name";
 const userName = localStorage.getItem(NAME_LS);
 
 
+function askName(){
+    h1.classList.remove('hide');
+    h1.classList.add('show');
+}
 
 function handelName(e){
     e.preventDefault();
     const nameValue = nameInput.value;
-    console.log('a');
 }
 
 function writeName(){
-// 이름 입력
-nameForm.addEventListener('submit',handelName);
+    // 이름 입력
+    nameForm.addEventListener('submit',handelName);
+    nameForm.classList.remove('show');
+    nameForm.classList.add('hide');
 }
 
-
 function loadName(){
-// 이름 저장
- if(userName !== null){
-    writeName();
-     h1.innerText = ` Do It !`
-}else{
-    console.log('null')
- }
- 
- 
- 
+    // 이름 저장
+    if(userName !== null){
+        writeName();
+        h1.innerText = ` Do It !`
+    }else{
+        askName();
+    }
 }
 
 function init(){
